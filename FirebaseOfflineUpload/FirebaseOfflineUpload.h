cc2bf20fa05d5ebd8e0a1579e1fc6f119ac30d96
Henry Hardy <hello@henryhardy.com.au>
2017-10-18 20:23:46 +1000

    Init

---
+++ FirebaseOfflineUpload/FirebaseOfflineUpload.h
@@ -0,0 +1,19 @@
+//
+//  FirebaseOfflineUpload.h
+//  FirebaseOfflineUpload
+//
+//  Created by Henry Hardy on 18/10/17.
+//  Copyright © 2017 Henry Hardy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FirebaseOfflineUpload.
+FOUNDATION_EXPORT double FirebaseOfflineUploadVersionNumber;
+
+//! Project version string for FirebaseOfflineUpload.
+FOUNDATION_EXPORT const unsigned char FirebaseOfflineUploadVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FirebaseOfflineUpload/PublicHeader.h>
+
+
